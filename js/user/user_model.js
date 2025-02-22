ec456b8a108655a4158274e0d78958887f762dc1
Jean <00zacks00@gmail.com>
2025-02-22 14:12:44 -0500

    feat:funcion para consultar dadtos de agente

--- js/user/user_model.js
+++ js/user/user_model.js
@@ -11,5 +11,13 @@ const Model = {
         return res
     },
 
+    async get_agent_info(id) {
+        const res = axios({
+            method: "GET",
+            url: "http://127.0.0.1:5000/get-agent-info/" + id,
+            headers: config.headers,
+        });
+        return res
+    }
 }
 export default Model;
