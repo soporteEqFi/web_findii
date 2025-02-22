f58554e84c916496e49bc4945e423487d1e49362
Jean <00zacks00@gmail.com>
2025-02-22 13:43:08 -0500

    feat: se agrega consulta de un usuario por cedula/id

---
+++ js/user/user_model.js
@@ -0,0 +1,15 @@
+import config from "../supabase/keys.js";
+
+const Model = {
+
+    async getUserInfo(id) {
+        const res = axios({
+            method: "GET",
+            url: "http://127.0.0.1:5000/get-user-info/" + id,
+            headers: config.headers,
+        });
+        return res
+    },
+
+}
+export default Model;
