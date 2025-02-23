2dfa1eb0bfee15056799b84f075506082dd40d8d
Jean <00zacks00@gmail.com>
2025-02-23 15:40:16 -0500

    feat: se agrega nuevo fetch de plantilla  para editar ventas

--- js/components/Modal/modal_views.js
+++ js/components/Modal/modal_views.js
@@ -1,13 +1,29 @@
-
-function modalContent(modalCuerpo, content_type) {
-    fetch(`../js/components/Modal/content_types/sells_content.html`)
-        .then(response => response.text())
-        .then(htmlContent => {
-            modalCuerpo.innerHTML = htmlContent;
-        })
-        .catch(error => {
-            console.error('Error loading HTML content:', error);
-        });
+const Modales = {
+    
+    modalContent(modalCuerpo, content_type) {
+        fetch(`../js/components/Modal/content_types/sells_content.html`)
+            .then(response => response.text())
+            .then(htmlContent => {
+                modalCuerpo.innerHTML = htmlContent;
+            })
+            .catch(error => {
+                console.error('Error loading HTML content:', error);
+            });
+    },
+    
+    editSellModal(modalCuerpo, dato) {
+        fetch('../js/components/Modal/content_types/edit_sell.html')
+            .then(response => response.text())
+            .then(template => {
+                let htmlContent = template    
+                modalCuerpo.innerHTML = htmlContent;
+            })
+            .catch(error => {
+                console.error('Error loading HTML template:', error);
+            });
+    }
 }
 
-export default modalContent;
+
+
+export default Modales;
