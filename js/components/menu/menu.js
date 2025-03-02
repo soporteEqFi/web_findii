04f2556ba53c82cf4aecded0cecf213388dd1c13
Jean <00zacks00@gmail.com>
2025-03-02 11:43:38 -0500

    fix: se arregla ruta de l  a página principal dependiendo del rol

--- js/components/menu/menu.js
+++ js/components/menu/menu.js
@@ -102,7 +102,7 @@ const Menu = {
         `
         opcionesMenu.innerHTML =
         `
-            <a href="agentes.html"><i class="fa-solid fa-house"></i> Inicio</a>
+            <a href="./agent.html"><i class="fa-solid fa-house"></i> Inicio</a>
             <a class="" href="./estadisticas.html"><i class="fa-solid fa-chart-simple"></i> Estadísticas</a>
             <a class="" href="./profile.html"><i class="fa-solid fa-user"></i> Perfil</a>
         `
