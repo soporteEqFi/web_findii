ee9ee1596791c997ad6537a500e2cbc70c5863bd
Jean <00zacks00@gmail.com>
2025-03-02 21:06:49 -0500

    feat: ahora se puede consultar los datos de un solicitante

--- js/user/user_model.js
+++ js/user/user_model.js
@@ -8,7 +8,6 @@ const Model = {
             url: "http://127.0.0.1:5000/get-user-info/" + id,
             headers: config.headers,
         });
-       
         return res
     },
 
@@ -20,8 +19,17 @@ const Model = {
         });
         return res
     },
-    async update_user(datos, idAgente){
 
+    async get_solicitante_info(id) {
+        const res = axios({
+            method: "GET",
+            url: "http://127.0.0.1:5000/get-solicitante-info/" + id,
+            headers: config.headers,
+        });
+        return res
+    },
+
+    async update_user(datos, idAgente){
         const data_agentes ={
             nombre: datos.nombre,
             cedula: datos.cedula,
@@ -29,7 +37,6 @@ const Model = {
             empresa: datos.empresa,
             id: idAgente
         }
-        
         const res = axios({
             method: "PUT",
             url: "http://127.0.0.1:5000/update-user/",
@@ -37,8 +44,6 @@ const Model = {
             headers: config.headers,
           });
           return res
-        
-        
     }
 }
 export default Model;
